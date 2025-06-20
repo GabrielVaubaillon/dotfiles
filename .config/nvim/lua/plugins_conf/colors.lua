686cac2ba638aa8bd90b7264746a29a31908f9ac
GabrielVaubaillon <gabriel.vaubaillon@gmail.com>
2025-06-20 10:48:06 +0100

    nvim show end of buffer

--- .config/nvim/lua/plugins_conf/colors.lua
+++ .config/nvim/lua/plugins_conf/colors.lua
@@ -1,6 +1,10 @@
 require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
     transparent_background = true,
+    show_end_of_buffer = true,
+    no_italic = false,
+    no_bold = false,
+    no_underline = false,
 })
 
 vim.cmd.colorscheme "catppuccin"
