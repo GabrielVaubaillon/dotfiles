9f71fd4ba876d9c7668e8282bd3cefb13522f8da
Gabriel Vaubaillon <gabriel.vaubaillon@gmail.com>
2025-07-10 15:06:37 +0100

    nvim remove italics

--- .config/nvim/lua/plugins_conf/colors.lua
+++ .config/nvim/lua/plugins_conf/colors.lua
@@ -2,7 +2,7 @@ require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
     transparent_background = true,
     show_end_of_buffer = true,
-    no_italic = false,
+    no_italic = true,
     no_bold = false,
     no_underline = false,
 })
